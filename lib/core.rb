629549df0265b96661c39961afd4dda8e78a577a
Filip Vrba <filipvrba@pm.me>
2023-03-19 20:08:08 +0100

    add vector

--- lib/core.rb
+++ lib/core.rb
@@ -1,8 +1,10 @@
 require 'lib/array.rb'
+
 require 'lib/core/version.rb'
 require 'lib/core/events.rb'
 require 'lib/core/basic_object.rb'
 require 'lib/core/scene.rb'
+require 'lib/core/vector2.rb'
 
 module Core
 end
