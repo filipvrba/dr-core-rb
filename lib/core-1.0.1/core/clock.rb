3d3fc13a356c05140cb82a51a547062e81c99285
Filip Vrba <filipvrba@pm.me>
2023-03-31 21:59:01 +0200

    add dt

--- lib/core-1.0.1/core/clock.rb
+++ lib/core-1.0.1/core/clock.rb
@@ -3,20 +3,20 @@ module Core
     def initialize
       @time = Time.now
       @fps_time = 1_000
-      @fix_dt = Number((1 / 60).toFixed(6))
+      @fix_dt = (1 / 60).round(6)
     end
   
     def delta_time(&callback)
-      current_time = Date.now()
+      current_time = Time.now
       dt = (current_time - @time) / @fps_time
   
       if dt > @fix_dt
         dt_count = (dt / @fix_dt).round
         dt_count.times do
-          callback(@fix_dt)
+          callback.call(@fix_dt) if callback
         end
       else
-        callback(@fix_dt)
+        callback.call(@fix_dt) if callback
       end
   
       @time = current_time
